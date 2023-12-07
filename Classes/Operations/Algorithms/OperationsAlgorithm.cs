338c5fd65247bcbf066cf31072d2669635c1e424
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-06 19:55:29 -0600

    .

---
+++ Classes/Operations/Algorithms/OperationsAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using DataStructuresAndAlgorithms_InCSharp.Classes.Operations.DataStructures;
+using DataStructuresAndAlgorithms_InCSharp.Interfaces.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms_InCSharp.Classes.Operations.Algorithms
+{
+    internal class OperationsAlgorithm
+    {
+        public void Algorithm(ImethodAlgorithms algorithm)
+        {
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out int option))
+                {
+                    OperationsList.Deffault(); continue;
+                }
+
+                switch (algorithm)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        OperationsList.Deffault();
+                        continue;
+                }
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
